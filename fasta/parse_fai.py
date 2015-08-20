ab33157fa4eff7540a17efad0683d98d86cdf1c4
Daniel Cook <danielecook@gmail.com>
2015-08-19 19:53:17 -0500

    added option to weight values

--- fasta/parse_fai.py
+++ fasta/parse_fai.py
@@ -1,6 +1,15 @@
 
-def parse_fai(fai):
+def parse_fai(fai, weight = False):
     """ Parses a fasta index file (produced by samtools index)
-    and returns chromosome names and lengths """
+    and returns chromosome names and lengths
+
+    Optionally generate weights
+    """
     with open(fai) as f:
-        return dict([x.strip().split("\t")[0:2] for x in f.readlines()])
+        chrom_length = [x.strip().split("\t")[0:2] for x in f.readlines()]
+        chrom_length = [[x[0],int(x[1])] for x in chrom_length]
+        if weight == True:
+            genome_length = sum([x[1] for x in chrom_length])
+            return dict([x[0],1.0*x[1]/genome_length] for x in chrom_length)
+        else:
+            return dict(chrom_length)
